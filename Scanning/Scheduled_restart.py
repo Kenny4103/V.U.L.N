7a8ed9397240f872fe492fba1d380bd9048acc1a
Diego <diegoesp710@gmail.com~>
2023-11-27 16:16:16 -0500

    here ar my changes still working on other things will commit them as soon as i am done.

---
+++ Scanning/Scheduled_restart.py
@@ -0,0 +1,25 @@
+import subprocess
+import schedule
+import time
+from datetime import datetime
+
+def run_script_b():
+    try:
+        # Run script_b.py using subprocess
+        subprocess.Popen(['sudo', 'python3', 'Database_reset.py'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
+        print("Database Reset!")
+    except subprocess.CalledProcessError as e:
+        print(f"Error executing script_b.py: {e}")
+
+def job():
+    print(f"Running job at {datetime.now()}")
+    run_script_b()
+
+if __name__ == "__main__":
+    schedule.every().day.at("16:03").do(job)
+
+    # Keep the script running to allow the scheduler to work
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+        print ("waiting")
